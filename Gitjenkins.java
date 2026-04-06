eccd9030151218402b18cb5cc86b13d94ae1b5f1
RohanDoke <rohan.doke24@ds.sce.edu.in>
2026-04-06 20:18:24 +0530

    Update Gitjenkins.java

--- Gitjenkins.java
+++ Gitjenkins.java
@@ -2,7 +2,8 @@ public class Gitjenkins {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins! and GitHub");
         System.out.println("I am Rohan");
-        System.out.println("Its my first Java program using Jenkins and GitHub and GIt");
+        System.out.println("Its my first Java program using Jenkins and GitHub and Git");
+         System.out.println("For running a Java Program");
     }
 }
 
