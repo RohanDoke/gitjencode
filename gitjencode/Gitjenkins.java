4dd46eaf2c8c3ac83fcd1b47181771369db0af19
RohanDoke <rohan.doke24@ds.sce.edu.in>
2026-04-06 19:54:06 +0530

    Add files via upload

---
+++ gitjencode/Gitjenkins.java
@@ -0,0 +1,8 @@
+public class Gitjenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins! and GitHub");
+        System.out.println("I am Rohan");
+        System.out.println("Its my first Java program using Jenkins and GitHub and GIt");
+    }
+}
+
